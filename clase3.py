af0561e63db9c8fa3fda89cba40e89fed1df7acf
konnyyd <czadiazg@gmail.com>
2025-01-08 21:47:09 -0300

    clase3.py

---
+++ clase3.py
@@ -0,0 +1,63 @@
+#REPETIR PALABRA ENUMERADO
+# for i in range(10):
+#     print ("saludo", i+1)
+#----------------------------------
+# total=0
+# for i in range(3):
+#     print("ingrese gasto")
+#     gasto = int(input())
+#     total=total+gasto
+# print(total)
+#--------------------------------------------
+#TABLA DE MULTLIPICAR
+# num=5
+# for i in range(10):
+#     print(num, "x", i+1, "=", (i+1)*num)
+#--------------------------------------------
+#SACAR PROMEDIO NOTAS
+# total = 0
+# for i in range (3):
+#     print("ingrese su nota")
+#     nota=int(input())
+#     total=total+nota/3
+# print ("el promedio es ", total)
+#--------------------------------------------
+#MOSTRAR NUMERO PAR-IMPAR
+# num=6
+# if num%2==0:
+#     print("el numero es par")
+# else:
+#     print ("el numero no es par")
+#-----------------------------------------------------------------
+#NUMERP IMPAR FORMA 1
+#i = int(input(ingrese hasta que numero quiere obtener impares))
+# for i in range (i):
+#     if i % 2 !=0:
+#         print(i)
+#LARGO DE UNA FRASE
+# palabra="we are the walking dead"
+# print(len(palabra))
+# for i in palabra:
+#     print(i)
+#------------------------------------------------------------------
+#LARGO DE FRASE Y QUE SE VALLA SUMANDO
+# total = 0
+# nombre=input()
+# for i in range(len(nombre)):
+#     total = total + (i+1) 
+# print("el total de la suma de los numeros es", total)
+#------------------------------------------------------------------
+#crear contraseña, solo 3 intentos
+# clave= "0567"
+# for i in range (3):
+#     clave=int(input("ingrese su clave"))
+#     if clave==clave:
+#         print("a ingresado con exito, bienvenido")
+#         break
+#     else:
+#         print("contraseña incorrecta, intente nuevamente")   
+# print("contraseña incorrecta, no le quedan mas intentos")
+#-----------------------------------------------------------------
+#numero misterioso
+numero_misterioso= "11"
+
